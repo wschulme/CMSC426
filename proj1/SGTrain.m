d7bbff4fcac1feaee01e4a1ce71f08f2aa71a4ba
wschulme <33583776+wschulme@users.noreply.github.com>
2020-09-14 21:15:13 -0400

    Basic Image folder, Outline for reading images from a folder

---
+++ proj1/SGTrain.m
@@ -0,0 +1,18 @@
+function SGTrain(folder)
+
+    % Constants
+    
+    selector = strcat(folder, '/*.jpg');
+    path = dir(selector);
+    imgN = length(path);
+    saveFileName = 'singleGaussModel.mat';
+    
+    % Training
+    
+    for i = 1:imgN
+        disp(path)
+        imgPath = fullfile(path(i).folder, path(i).name);
+        img = imread(imgPath);
+        imshow(img);
+    end
+end
---
+++ proj1/gaussian.m
@@ -0,0 +1,10 @@
+clear
+clc
+close all
+
+%% Train
+SGTrain('train_basic_image');
+
+%% Predict
+
+%% Other Shit, Presumably
