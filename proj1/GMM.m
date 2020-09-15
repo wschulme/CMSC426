924d4535044cab93da12190ec4b361fe8a701ada
wschulme <33583776+wschulme@users.noreply.github.com>
2020-09-14 20:21:34 -0400

    More skeletons

---
+++ proj1/GMM.m
@@ -0,0 +1,16 @@
+function [cluster, d] = gmm(training)
+    theta = 0.01; % Arbitrary
+    K = 10; % Arbitrary
+    clusters = {};
+    if training
+        %load somehow
+        trainGMM(K);
+    else
+        % load the testing images somehow
+        cluster = testGMM(Model, theta);
+    end
+    d = measureDepth(cluster);
+    plotGMM(model);
+    cluster = cluster;
+    
+end
