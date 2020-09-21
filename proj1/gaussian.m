e5379fe08c7e8d24015cb2b0975a7f394ba5f4ea
wschulme <33583776+wschulme@users.noreply.github.com>
2020-09-21 17:45:14 -0400

    Fixes masking procedure

--- proj1/gaussian.m
+++ proj1/gaussian.m
@@ -2,10 +2,6 @@ clear
 clc
 close all
 
-%% Train
-% SGTrain('train_basic_image');
-
-%% Predict
 % Grab basic image folder
 selector = strcat('train_basic_image', '/*.jpg');
 path = dir(selector);
@@ -17,25 +13,30 @@ for i = 1:imgN
     imgPath = fullfile(path(i).folder, path(i).name);
     I = imread(imgPath);
     imshow(I);
+    
+    % Get Dims
+    sz = size(I);
+    height = I(1);
+    width = I(2);
+
+    % Get Mask
+    BW = uint8(roipoly(I));
+
+    %Mask
+    r = I(:,:,1);
+    g = I(:,:,2);
+    b = I(:,:,3);
+
+    maskedI = uint8(zeros(size(I))); % Initialize
+    maskedI(:,:,1) = r .* BW;
+    maskedI(:,:,2) = g .* BW;
+    maskedI(:,:,3) = b .* BW;
+
+    imshow(maskedI);
+
+    % Get Mean, Covariance, Probability
+    r = maskedI(:,:,1);
+    g = maskedI(:,:,2);
+    b = maskedI(:,:,3);
+    
 end
-
-% Get Dims
-sz = size(I);
-height = I(1);
-width = I(2);
-
-% Get Mask
-BW = roipoly(I);
-r = I(:,:,1);
-g = I(:,:,2);
-b = I(:,:,3);
-BWmat = [r g b]; % Need help getting rgb matrix from BW :)
-x = [206 264 262 202];
-y = [324 324 388 387];
-BW = roipoly(I,x,y);
-imshow(BW)
-% Get Mean, Covariance, Probability
-ballmean = mean(BWmat);
-%ballcov =
-%ballprob =
-%% Other Shit, Presumably
