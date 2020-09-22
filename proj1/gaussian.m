c6adad1194138658a53b7cd073bfdba9348ffc9d
wschulme <33583776+wschulme@users.noreply.github.com>
2020-09-21 23:09:23 -0400

    Added toggle for loading vs training

--- proj1/gaussian.m
+++ proj1/gaussian.m
@@ -2,6 +2,11 @@ clear
 clc
 close all
 
+%% Train Constant
+
+%Change to false to load from singleGaussModel.mat
+TRAIN = false;
+
 %% Initialize
 
 %Note, this folder can be made into more images. This file is scalable.
@@ -12,78 +17,82 @@ saveFileName = 'singleGaussModel.mat';
 
 %% Grab All Orange Pixels from Training Data
 
-orange = [];
-for i = 1:imgN
-    disp(path)
-    imgPath = fullfile(path(i).folder, path(i).name);
-    I = imread(imgPath);
-    imshow(I);
-    
-    % Get Dims
-    sz = size(I);
-    height = sz(2);
-    width = sz(1);
+if(TRAIN)
+    orange = [];
+    for i = 1:imgN
+        disp(path)
+        imgPath = fullfile(path(i).folder, path(i).name);
+        I = imread(imgPath);
+        imshow(I);
 
-    % Get Mask
-    BW = uint8(roipoly(I));
+        % Get Dims
+        sz = size(I);
+        height = sz(2);
+        width = sz(1);
 
-    %Mask
-    r = I(:,:,1);
-    g = I(:,:,2);
-    b = I(:,:,3);
-    
-    %Lowkey you don't NEED this masked image but it is good for
-    %visualization.
-    maskedI = uint8(zeros(size(I))); 
-    maskedI(:,:,1) = r .* BW;
-    maskedI(:,:,2) = g .* BW;
-    maskedI(:,:,3) = b .* BW;
+        % Get Mask
+        BW = uint8(roipoly(I));
 
-    imshow(maskedI);
-    
-    %Add all found oranges
-    nO = 0;
-    maskedI = double(maskedI);
-    for x = 1:width
-        for y = 1:height
-            if BW(x,y) == 1
-                %Add to list of "oranges" pixels
-                orange = [orange reshape(maskedI(x,y,:),3,1)];
-                %Increment how many "orange" pixels counted
-                nO = nO+1
+        %Mask
+        r = I(:,:,1);
+        g = I(:,:,2);
+        b = I(:,:,3);
+
+        %Lowkey you don't NEED this masked image but it is good for
+        %visualization.
+        maskedI = uint8(zeros(size(I))); 
+        maskedI(:,:,1) = r .* BW;
+        maskedI(:,:,2) = g .* BW;
+        maskedI(:,:,3) = b .* BW;
+
+        imshow(maskedI);
+
+        %Add all found oranges
+        nO = 0;
+        maskedI = double(maskedI);
+        for x = 1:width
+            for y = 1:height
+                if BW(x,y) == 1
+                    %Add to list of "oranges" pixels
+                    orange = [orange reshape(maskedI(x,y,:),3,1)];
+                    %Increment how many "orange" pixels counted
+                    nO = nO+1
+                end
             end
         end
     end
-end
 
-%% Calculate Mean and Covariance
+    %% Calculate Mean and Covariance
 
-% mu = [r, g, b]
-mu = double(zeros(3,1));
+    % mu = [r, g, b]
+    mu = double(zeros(3,1));
 
-for i=1:nO
-   mu = mu + orange(:,i);
-end
+    for i=1:nO
+       mu = mu + orange(:,i);
+    end
 
-mu = mu/nO;
-disp("Empirical Mean");
-disp(mu);
+    mu = mu/nO;
+    disp("Empirical Mean");
+    disp(mu);
 
-sigma = double(zeros(3,3));
+    sigma = double(zeros(3,3));
 
-for i=1:nO
-   a = orange(:,i)-mu;
-   sigma = sigma + (a * a');
-end
+    for i=1:nO
+       a = orange(:,i)-mu;
+       sigma = sigma + (a * a');
+    end
 
-sigma = sigma/nO;
-disp("Empirical Covariance");
-disp(sigma);
+    sigma = sigma/nO;
+    disp("Empirical Covariance");
+    disp(sigma);
 
-%% Save Data
+    %% Save Data
 
-%Save mean and cov
-save(saveFileName, 'mu', 'sigma');
+    %Save mean and cov
+    save(saveFileName, 'mu', 'sigma');
+else
+    load(saveFileName, 'mu', 'sigma');
+end 
 
 %% TODO: Predict! Literally just plug it in
 
