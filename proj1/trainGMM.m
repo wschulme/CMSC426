3cd6cbd52489c859ee2250d7a3a2ea2225df139c
Sarah Bang <sarahbang9799@gmail.com>
2020-09-25 16:30:45 -0400

    trainGMM set up

--- proj1/trainGMM.m
+++ proj1/trainGMM.m
@@ -1,10 +1,30 @@
 function trainGMM(K)
-    e = 0; % convergence criteria
+    clear
+    clc
+    close all
+
+    selector = strcat('train_images', '/*.jpg');
+    path = dir(selector);
+    imgN = length(path);
+    saveFileName = 'GMMmodel.mat';
+    
+    % TODO: determine convergence criteria
+    E = 0; % convergence criteria
+ 
+    % TODO: initialize all variables below using train
     % Initialize Model; scaling factor, gaussian mean & covariance
-    while (i <= max_iters) && (abs(Mean-prevMean) > e)
+    load(saveFileName, 'pie', 'mu', 'sigma'); % I put it as pie since matlab has preexisting pi
+    max_iters = 5;
+    weight = zeros();   % cluster weight, ??? dimensions
+    
+    while (it <= max_iters) && (abs(mu-prevMu) > E)
         expectation_step
+        % TODO: E-step, j = data point idx and i = cluster idx
+        
         maximization_step
-        i++;
+        % TODO: M-step
+        
+        it++;
     end
     % Save Model
 end
