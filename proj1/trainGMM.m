7ee46141ef659f3e003e95e4d1fb2ced934d2623
Sarah Bang <sarahbang9799@gmail.com>
2020-09-28 01:23:58 -0400

    m step gmmtrain

--- proj1/trainGMM.m
+++ proj1/trainGMM.m
@@ -1,5 +1,4 @@
 function trainGMM(K)
-    clear
     clc
     close all
 
@@ -8,108 +7,100 @@ function trainGMM(K)
     imgN = length(path);
     saveFileName = 'GMMmodel.mat';
     
-    % TODO: determine convergence criteria
-    %E = 0; % convergence criteria
+    orange = [];
+    % For each image
+    for i = 1:1
+        disp(path)
+        imgPath = fullfile(path(i).folder, path(i).name);
+        I = imread(imgPath);
+        imshow(I);
+
+        % Get Dims
+        sz = size(I);
+        width = sz(1);
+        height = sz(2);
+
+        % Form Mask
+        BW = uint8(roipoly(I));
+
+        % Get RGB values for image
+        r = I(:,:,1);
+        g = I(:,:,2);
+        b = I(:,:,3);
+
+        %Lowkey you don't NEED this masked image but it is good for
+        %visualization. Just applying the mask to the image.
+        maskedI = uint8(zeros(size(I))); 
+        maskedI(:,:,1) = r .* BW;
+        maskedI(:,:,2) = g .* BW;
+        maskedI(:,:,3) = b .* BW;
+
+        %imshow(maskedI);
+
+        % Look at every pixel. If it's 1 in the roipoly image, it's orange.
+        % Add it to the running list of orange pixels.
+        nO = 0;
+        maskedI = double(maskedI);
+        for x = 1:width
+            for y = 1:height
+                if BW(x,y) == 1
+                    %Add to list of oranges pixels
+                    orange = [orange reshape(maskedI(x,y,:),3,1)];
+                    %Increment how many orange pixels counted
+                    nO = nO+1;
+                end
+            end
+        end
+    end
  
-    % TODO: initialize all variables below using train
-    % Initialize Model; scaling factor, gaussian mean & covariance
-    %load(saveFileName, 'pie', 'mu', 'sigma'); % I put it as pie since matlab has preexisting pi
-    max_iters = 7;
-    prior = .5;
-    %weight = zeros();   % cluster weight, ??? dimensions
+    K = 3;
     
     e = 0.0001; % convergence criteria
-    pie = zeros(1,K);
-    mu = zeros(1,K);  
-    sigma = zeros(1,K);  
-
+    pie = rand(K,1);
+    mu = rand(K,3);  
+    sigma = 100*(reshape(repmat(diag(ones(3,1)),1,K),[3, 3, K]));
+    alpha = zeros(K,nO);
+    maxIter = 10;
+    iter = 1;
     
-    % Initialize each gaussian with their own pie,mu,sigma in list form
-    for g = 1:K
-        % For each gaussian, we initially start with a random init of
-        % mu, pie and sigma ie. scaling factor, mean and co-variance
-        pie{g} = rand(3,1); 
-        mu{g} = 1 + (K-1)*rand(1,1); 
-        sigma{g} = rand(3);
-    end
-    
-    %a_ij is weight for data point j and gaussian model i
-    while (i <= max_iters) && (abs(mu-prevMu) > e) % these are still wrong
-    
-        % E step
-        % Go through for each image
-        for img = 1:imgN
+    while iter < maxIter
+        %% Expectation
+        for o = 1:nO
+            for i = 1:K
+                ex = [orange(1,o) orange(2,o) orange(3,o)];
+                a = pie(i)*mvnpdf(ex, mu(i), sigma(:,:,i));
 
-            %For each image we are going to create a (x, y) matrix for each
-            %pixel, where each pixel will have an array of items of length K
-            %for the number of gaussians
-            imgPath = fullfile(path(img).folder, path(img).name);
-            I = imread(imgPath);
-
-            % Get Dims
-            sz = size(I);
-            width = sz(1);
-            height = sz(2);
-
-            % Gaussian array
-            gaus = zeros(width, height, K);
-
-            % We go through each pixel to get the different gaussians
-            for x=1:width
-                for y=1:height
-                    % Total gaussian is the bottom part of the 
-                    total_gaussian = 0 %what shape??? Is this just an int?
-
-                    % For each different gaussian
-                    for g = 1:K
-
-                        %Form RGB value
-                        ex = [double(I(x,y,1)); double(I(x,y,2)); double(I(x,y,3))];
-                        l = likelihood(ex, sigma, mu, 3);
-                        %p = prob(l,prior);
-                        %Does our model consider it orange?
-                        %if p >= threshold
-                            %If so, color it white in the prediction
-                            %prediction(x,y) = 1;
-                        %end
-
-                        % We save the numerator of a_ij first so we can
-                        % divide later
-                        gaus(x,y,g) = pie{K}*l;
-                        total_gaussian = total_gaussian + pie{K}*l;
-
-                    end
-
-                    % We finally divide the pixel's individual gaussian by
-                    % the total
-                    a_ij = I(x,y,:)./total_gaussian;
-
+                sum_a = 0;
+                for cluster = 1:K
+                    sum_a = sum_a + pie(cluster)*mvnpdf(ex, mu(cluster), sigma(:,:,cluster));
                 end
 
+                alpha(i,o)= a/sum_a;
             end
+        end
 
+        %% Maximization 
+        temp = zeros(1,K);
+        temp = sum(alpha');
+        
+        % Calculate mu
+        for i=1:K
+            sum_mu = 0;
+            for o = 1:nO
+                sum_mu = sum_mu + (alpha(i,o)*orange(:,o));
+            end
+            mu(:,i) = sum_mu/temp(i);
         end
         
-        % M step
+        % Calculate sigma
+        for i=1:K
+            sum_sig = 0;
+            for o = 1:nO
+                sum_sig = sum_sig + alpha(i,o)*(orange(:,o)-mu(:,i))*(orange(:,o)-mu(:,i))';
+            end
+            sigma(:,:,i) = sum_sig/temp(i);
+        end
         
-    
+        iter = iter+1;
     end
-    
-end
-
-% Helper Functions
-
-% Bayes Rule (aka Posterior)
-function p = prob(likelihood, prior)
-    %top = likelihood * prior;
-    %bottom = (likelihood * prior) + (likelihood * (1-prior));
-    %p = top / bottom;
-    p = likelihood * prior;
-end
-
-% Calculate Likelihood
-function l = likelihood(x,sigma,mu,N)
-    a = 1/(sqrt((2*pi)^N*det(sigma)));
-    b = exp(-.5*(x-mu)'*(sigma\(x-mu)));
-    l = a*b;
 end
