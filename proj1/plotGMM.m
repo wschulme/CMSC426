077d97725cd4320bd6152c1ed080d1ae6059a550
yashashripendse <47879638+yashashripendse@users.noreply.github.com>
2020-09-30 21:48:55 -0400

    3d now

--- proj1/plotGMM.m
+++ proj1/plotGMM.m
@@ -5,7 +5,9 @@ function plotGMM()
     
     hold on;
     for i=1:K
-        plot_gaussian_ellipsoid(mu(i,:), sigma{i})
+        plot_gaussian_ellipsoid(mu(i,:), sigma{i});
     end
+    view(100,36); set(gca,'proj','perspective'); grid on; 
+    grid on; axis equal; axis tight;
     hold off;
 end
