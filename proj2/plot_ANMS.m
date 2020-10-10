31a30823bd21462fd3b24156ff9b40e8ce7f03ab
Brian Chin <bchin@Brians-MBP.fios-router.home>
2020-10-10 18:25:51 -0400

    started plot_ANMS

---
+++ proj2/plot_ANMS.m
@@ -0,0 +1,29 @@
+% plot ANMS 
+
+function p = plot_ANMS(I, NStrong)
+    points = detectHarrisFeatures(I)
+    
+    strongest = points.selectStrongest(NStrong);
+    imshow(I)
+    hold on
+    plot(strongest)
+
+    r = zeros(NStrong)
+    
+    for i = 1:NStrong
+        for j = 1:NStrong
+           if C_img(y(j)), x(j)) > C_img(y(i), x(i))
+             ED = (y(j) - y(i))^2 + (x(j) - x(i))^2
+             if (ED < radius(i))
+               radius(i) = ED;
+             end
+           end
+        end
+    end
+    
+    [RadiusValue, RadiusIdx] = sort(radius, 'descend')
+    x = x(RadiusIdx(1:NBest))
+    y = y(RadiusIdx(1:NBest))
+    p = [y,x]
+end
+
