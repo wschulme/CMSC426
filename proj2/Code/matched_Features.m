84911d3058e823792a8fcb96518faf55113e322c
Brian Chin <bchin@Brians-MBP.fios-router.home>
2020-10-11 14:51:43 -0400

    merged files together

--- proj2/Code/matched_Features.m
+++ proj2/Code/matched_Features.m
@@ -1,9 +1,32 @@
-%run plot_ANMS
+% Feature Descriptor
+function main()
+    selector = strcat('./Images/Set1', '/*.jpg');
+    path = dir(selector);
+    
+    match(path)
+end
+
+function match(path)
+    imgN = length(path);
+    plots = zeros(2)
+    
+    %run plot_ANMS
+    for i = 1:2
+        imgPath = fullfile(path(i).folder, path(i).name);
+        I = rgb2gray(imread(imgPath));
+        %plot_corners(I, 150);
+        H = fspecial('disk',40);
+        blurred = imfilter(I,H,'replicate'); 
+        imshow(blurred);
+    end
+end
+
+
 %shows match
-[f1, vpts1] = extractFeatures(I1, points1);
-[f2, vpts2] = extractFeatures(I2, points2);
-indexPairs = matchFeatures(f1, f2) ;
-matchedPoints1 = vpts1(indexPairs(1:20, 1));
-matchedPoints2 = vpts2(indexPairs(1:20, 2));
+% [f1, vpts1] = extractFeatures(I1, points1);
+% [f2, vpts2] = extractFeatures(I2, points2);
+% indexPairs = matchFeatures(f1, f2) ;
+% matchedPoints1 = vpts1(indexPairs(1:20, 1));
+% matchedPoints2 = vpts2(indexPairs(1:20, 2));
 
    
