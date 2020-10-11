bc12a6526f3e008eb6e3a3e5b0da6b87a9d77063
Brian Chin <bchin@Brians-MBP.fios-router.home>
2020-10-10 23:22:36 -0400

    debugging plot_ANMS

--- proj2/matched_Features.m
+++ proj2/matched_Features.m
@@ -6,40 +6,4 @@ indexPairs = matchFeatures(f1, f2) ;
 matchedPoints1 = vpts1(indexPairs(1:20, 1));
 matchedPoints2 = vpts2(indexPairs(1:20, 2));
 
-    features = detectHarrisFeatures(I);
-    points = features.Location
-    
-    Irm = imregionalmax(points)
-    x = points(:,1)
-    y = points(:,2)
-    
-    %strongest = features.selectStrongest(NStrong);
-    imshow(I)
-    hold on
-    plot(features)
-    %plot(strongest)
-    hold off
-
-    radius = Inf(NStrong);
-
-    for i = 1:NStrong
-     for j = 1:NStrong
-         
-        if C_img((y(j)), x(j)) > C_img(y(i), x(i))
-          ED = (y(j) - y(i))^2 + (x(j) - x(i))^2
-          if (ED < radius(i))
-            radius(i) = ED;
-          end
-        end
-     end
-    end
-
-    [RadiusValue, RadiusIdx] = sort(radius, 'descend')
-    x = x(RadiusIdx(1:NBest))
-    y = y(RadiusIdx(1:NBest))
-    p = [y,x]
-    
-    imshow(I)
-    hold on
-    plot(p)
-    hold off
+   
