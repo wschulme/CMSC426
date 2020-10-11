6fbe989c700be408e67348d8b54ac36c3e2f5d1c
Brian Chin <bchin@Brians-MBP.fios-router.home>
2020-10-11 16:58:02 -0400

    deleted unneed code from feature_descriptor

--- proj2/Code/feature_descriptor.m
+++ proj2/Code/feature_descriptor.m
@@ -23,12 +23,4 @@ function f = features(path)
     end
 end
 
-
-%shows match
-% [f1, vpts1] = extractFeatures(I1, points1);
-% [f2, vpts2] = extractFeatures(I2, points2);
-% indexPairs = matchFeatures(f1, f2) ;
-% matchedPoints1 = vpts1(indexPairs(1:20, 1));
-% matchedPoints2 = vpts2(indexPairs(1:20, 2));
-
    
