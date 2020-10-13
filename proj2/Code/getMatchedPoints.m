ab68ea9969c86d8a8f156a310a33fd75239544ea
Sarah Bang <sarahbang9799@gmail.com>
2020-10-13 00:12:09 -0400

    Update getMatchedPoints.m

--- proj2/Code/getMatchedPoints.m
+++ proj2/Code/getMatchedPoints.m
@@ -1,8 +1,10 @@
 function [match1,match2] = getMatchedPoints(d1, d2, p1, p2, thresh)
     match1 = zeros(1,2);
     match2 = zeros(1,2);
+    [sz,sz2] = size(d1);
+    
     % Pick 1 point in image 1
-    for i = 1:64
+    for i = 1:sz
         % Compute sum of square difference between all points in image 2
         for j = 1:64
             sumsq(j) = sum((d1(i,:)-d2(j,:)).^2);    
@@ -15,7 +17,7 @@ function [match1,match2] = getMatchedPoints(d1, d2, p1, p2, thresh)
         % TODO: fix coordinates
         if((oneMatch/twoMatch) < thresh)
             match1 = vertcat(match1, [p1(i,1) p1(i,2)]);
-            match2 = vertcat(match2, [p2(N(sz),1) p2(N(sz),2)]);
+            match2 = vertcat(match2, [p2(N(1),1) p2(N(1),2)]);
         end
     end
 end
