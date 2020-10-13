1c7f2d5b1633dee980a14405e0b5d32a4247160b
wschulme <33583776+wschulme@users.noreply.github.com>
2020-10-13 00:03:31 -0400

    Style!

---
+++ proj2/Code/getMatchedPoints.m
@@ -0,0 +1,21 @@
+function [match1,match2] = getMatchedPoints(d1, d2, p1, p2, thresh)
+    match1 = zeros(1,2);
+    match2 = zeros(1,2);
+    % Pick 1 point in image 1
+    for i = 1:64
+        % Compute sum of square difference between all points in image 2
+        for j = 1:64
+            sumsq(j) = sum((d1(i,:)-d2(j,:)).^2);    
+        end
+        % Get 2 lowest distance aka 2 BEST distance
+        [M,N] = sort(sumsq);
+        oneMatch = M(1);
+        twoMatch = M(2);
+        % Keep the matched pair if below ratio thresh, else reject
+        % TODO: fix coordinates
+        if((oneMatch/twoMatch) < thresh)
+            match1 = vertcat(match1, [p1(i,1) p1(i,2)]);
+            match2 = vertcat(match2, [p2(N(1),1) p2(N(1),2)]);
+        end
+    end
+end
