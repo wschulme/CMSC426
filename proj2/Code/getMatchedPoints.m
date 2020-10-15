c5921909c1b7e969bb014aee16febb11ceb04822
Sarah Bang <sarahbang9799@gmail.com>
2020-10-14 23:21:11 -0400

    matchedpoints fix

--- proj2/Code/getMatchedPoints.m
+++ proj2/Code/getMatchedPoints.m
@@ -1,15 +1,13 @@
 function [match1,match2] = getMatchedPoints(d1, d2, p1, p2, thresh)
-    numFeatures = length(d1(1,:));
-    disp(numFeatures);
+    sz1 = length(d1(1,:));
+    sz2 = length(d2(2,:));
     match1 = [];
     match2 = [];
-    for i= 1:numFeatures
-        feature = d1(:, i);
-        
-        sqrDiff = (feature - d2).^2;
-        sumSqr = sum(sqrDiff, 2);
+    for i = 1:sz1
+        for j = 1:sz2
+            sumSqr(j) = sum((d1(:,i) - d2(:,j)).^2);
+        end
         [sortedDist, Idx] = sort(sumSqr);
-        
         ratio = sortedDist(1)/sortedDist(2);
         if (ratio < thresh)
             match1 = [match1 ; p1(i,:)];
