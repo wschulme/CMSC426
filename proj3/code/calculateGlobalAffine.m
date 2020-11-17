c0e4b35154178c8784e5e7c4ca214df698e30d29
bchin1 <bqin.9215@gmail.com>
2020-11-16 21:57:36 -0500

    Prototyped part 4.1 and 4.2

--- proj3/code/calculateGlobalAffine.m
+++ proj3/code/calculateGlobalAffine.m
@@ -1,5 +1,28 @@
 function [WarpedFrame, WarpedMask, WarpedMaskOutline, WarpedLocalWindows] = calculateGlobalAffine(IMG1,IMG2,Mask,Windows)
 % CALCULATEGLOBALAFFINE: finds affine transform between two frames, and applies it to frame1, the mask, and local windows.
+    ptsOriginal  = detectSURFFeatures(IMG1);
+    ptsS = detectSURFFeatures(IMG2);
+    
+    % returns extracted feature vectors, and their corresponding locations, from an image.
+    [featuresOriginal,validPtsOriginal] = extractFeatures(IMG1,ptsOriginal);
+    [featuresDistorted,validPtsDistorted] = extractFeatures(IMG2,ptsShifted);
+    index_pairs = matchFeatures(featuresOriginal,featuresDistorted);
+    matchedPtsOriginal  = validPtsOriginal(index_pairs(:,1));
+    matchedPtsDistorted = validPtsDistorted(index_pairs(:,2));
+    
+    [tform,inlierIdx] = estimateGeometricTransform(matchedPtsDistorted, matchedPtsOriginal, 'similarity');
+    %initial shape alignment usually captures and compensates for large rigid motions of the foreground object
+    rebounded = imwarp(distorted,tform,'OutputView',outputView);
+
+    % I have no idea if the bottom is right
+    
+    opticFlow = opticalFlowHS
+    
+    for i = 1:length(Windows)
+        frameGray = rgb2gray(Windows{i});  
+        flow = estimateFlow(opticFlow,frameGray);
+        % Need to apply the opticFlow shift to the mask and windows
+    end
     
 end
 
