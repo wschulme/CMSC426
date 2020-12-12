321459a6ba9979dc7d82b094cd40a5cfeb7667ce
wschulme <33583776+wschulme@users.noreply.github.com>
2020-12-12 18:12:44 -0500

    Homography and pose components

--- proj4/SLAMusingGTSAM.m
+++ proj4/SLAMusingGTSAM.m
@@ -1,6 +1,5 @@
 function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSize)
 	% For Input and Output specifications refer to the project pdf
-
 	import gtsam.*
 	% Refer to Factor Graphs and GTSAM Introduction
 	% https://research.cc.gatech.edu/borg/sites/edu.borg/files/downloads/gtsam.pdf
@@ -28,13 +27,21 @@ function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSi
     %% Pre-GTSAM
     %Calculate initial homography assuming a planar carpet of April Tags
     imageCoords = [Tag10.p1; Tag10.p2; Tag10.p3; Tag10.p4];
-    worldCoords = [[0,0];[TagSize,0];[0,TagSize];[TagSize,TagSize]];
+    worldCoords = [[0,0,1];[TagSize,0,1];[0,TagSize,1];[TagSize,TagSize,1]];
     
-    H = est_homography(worldCoords(:,1), worldCoords(:,2), imageCoords(:,1), imageCoords(:,2));
+    H = getHomography(worldCoords, imageCoords);
+    [R,T] = getPoseParts(K,H);
+    %pose = getPoseRow(R,T);
     
-    %Proof of concept. Does this homography get us from image to world?
-    [X,Y] = apply_homography(H, imageCoords(:,1), imageCoords(:,2));
-    disp([X,Y]);
+    %Sanity check
+    disp(imageCoords);
+    results = H*worldCoords';
+    disp(results(:,1)./results(3,1));
+    disp(results(:,2)./results(3,2));
+    disp(results(:,3)./results(3,3));
+    disp(results(:,4)./results(3,4));
+    
+    
     
 end
 
@@ -44,4 +51,38 @@ Detection.p1 = [det(2), det(3)];
 Detection.p2 = [det(4), det(5)];
 Detection.p3 = [det(6), det(7)];
 Detection.p4 = [det(8), det(9)];
+end
+
+function [R,T] = getPoseParts(K,H)
+    KH = inv(K)*H;
+    temp = [KH(:,1) KH(:,2) cross(KH(:,1),KH(:,2))];
+    [U,~,V] = svd(temp);
+    R = U*[1 0 0; 0 1 0; 0 0 det(U*V.')]*V.';
+    T = KH(:,3)/norm(KH(:,1),2);
+    %Note: Dan has this as -T. We will need to see if it works like this or
+    %+T
+    T = R'*(-T);
+end
+
+function H = getHomography(X,x)
+    % X: 3x4 World
+	% x: 2x4 Image
+    X = X';
+    x = x';
+    A = [X(:,1).' 0 0 0 -X(:,1).'*x(1,1);
+         0 0 0 X(:,1).' -X(:,1).'*x(2,1);
+         X(:,2).' 0 0 0 -X(:,2).'*x(1,2);
+         0 0 0 X(:,2).' -X(:,2).'*x(2,2);
+         X(:,3).' 0 0 0 -X(:,3).'*x(1,3);
+         0 0 0 X(:,3).' -X(:,3).'*x(2,3);
+         X(:,4).' 0 0 0 -X(:,4).'*x(1,4);
+         0 0 0 X(:,4).' -X(:,4).'*x(2,4)];
+    [~, ~, V] = svd(A.'*A);
+    rawH = reshape(V(:,9),3,3)';
+    H = rawH/(rawH(3,3));
+end
+
+function pose = getPoseRow(R,T)
+    %TODO: how tf get quaternions????
+    pose = horzcat([T(1), T(2), T(3)], rotm2quat(R));
 end
